0d99debd9781df68fdbc10eb249f4912e7a28b33
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-05-27 14:09:36 +0200

    Add files via upload

--- blood-doner_system.sql
+++ blood-doner_system.sql
@@ -1,7 +1,17 @@
-CREATE DATABASE db_blood_donor_system;
-USE db_blood_donor_system;
-CREATE TABLE patient(
-unquie_id INT NOT NULL PRIMARY KEY,
-p_first_name VARCHAR(255),
+SELECT * FROM patient_in_need_of_blood;
+INSERT INTO patient_in_need_of_blood(unquie_id, p_first_name,p_last_name,blood_group,diease, injury) VALUES(1001, "Jane",  "Doe","A-", "none", "gunshot wounds");
+SELECT * FROM patient_in_need_of_blood;
+INSERT INTO patient_in_need_of_blood(unquie_id, p_first_name,p_last_name,blood_group,diease, injury)VALUES(1005, "Sarah", "Hart", "B-", "cancer", "none");
+SELECT * FROM patient_in_need_of_blood;
+INSERT INTO patient_in_need_of_blood(unquie_id, p_first_name,p_last_name,blood_group,diease, injury)VALUES(1007, "John", "Doe" , "B+", "none", "excesive blood in vomit");
+SELECT * FROM patient_in_need_of_blood;
+SELECT * FROM patient_in_need_of_blood;
+INSERT INTO donor(d_unquie_id, d_first_name, d_last_name, d_age, blood_group, contact_number)VALUES(10087, "Brad", "Musk", 30, "AB-", 011234654719);
+SELECT * FROM donor;
+UPDATE patient_in_need_of_blood 
+SET diease = "lymphola" WHERE unquie_id = 1007;
+SELECT *FROM patient_in_need_of_blood ;
 
-)
+SELECT * FROM donor;
+INSERT INTO donor(d_unquie_id, d_first_name, d_last_name, d_age, blood_group, contact_number) VALUES(1123, "John", "Snow", 24, "0+", 0891234567);
+SELECT * FROM donor;
